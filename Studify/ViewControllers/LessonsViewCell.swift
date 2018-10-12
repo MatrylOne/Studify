72dafc7af96688b7b746b7a11e2e83171c13c091
MatrylOne <matryl1@me.com>
2018-10-12 14:48:37 +0200

    Added Lessons cell view

---
+++ Studify/ViewControllers/LessonsViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  LessonsViewCell.swift
+//  Studify
+//
+//  Created by Jakub Nadolny on 12/10/2018.
+//  Copyright © 2018 Jakub Nadolny. All rights reserved.
+//
+
+import UIKit
+
+class LessonsViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var color: UIView!
+    @IBOutlet weak var image: UIImageView!
+}
