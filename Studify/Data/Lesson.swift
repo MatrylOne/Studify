6e8bc56aae14083b687933f1a61da9b39e97b4a0
MatrylOne <matryl1@me.com>
2018-10-16 14:28:18 +0200

    Added Lesson data structure

---
+++ Studify/Data/Lesson.swift
@@ -0,0 +1,16 @@
+//
+//  Lesson.swift
+//  Studify
+//
+//  Created by Jakub Nadolny on 15/10/2018.
+//  Copyright © 2018 Jakub Nadolny. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Lesson{
+    let name:String
+    let description:String
+    let instruction:String
+}
